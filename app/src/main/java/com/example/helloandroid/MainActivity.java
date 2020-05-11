c22befd78df83db7b486276eb782d7b9e7509893
Leyton <leytomio17@gmail.com>
2020-05-11 15:03:50 +0200

    first

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView nachricht;
+    private Button weiterFertig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nachricht = findViewById(R.id.nachricht);
+        weiterFertig = findViewById(R.id.weiter_fertig);
+
+        nachricht.setText(R.string.willkommen);
+        weiterFertig.setText(R.string.weiter);
+
+    }
+}
